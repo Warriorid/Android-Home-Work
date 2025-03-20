3eff6ab0b406ea48103f95980faf24eb84e92a37
Warriorid <i.hockeyakk@fmail.com>
2025-03-20 18:35:11 +0300

    The issues have been fixed (hw4)

--- app/src/main/java/com/example/myapplication/Main.kt
+++ app/src/main/java/com/example/myapplication/Main.kt
@@ -2,29 +2,29 @@ package com.example.myapplication
 
 fun main(){
     val listOfBooks = listOf(
-        Book("Война и мир", 1000, "Л. Н. Толстой", 1, true),
-        Book("Капитанская дочка", 250, "А. C. Пушкин", 2, true),
-        Book("Преступление и наказание", 430, "Ф. М. Достоевский", 3, false),
-        Book("Мастер и Маргарита", 480, "М. А. Булгаков", 4, true),
-        Book("Анна Каренина", 864, "Л. Н. Толстой", 5, true),
-        Book("Евгений Онегин", 320, "А. С. Пушкин", 6, true)
+        Book("Война и мир", 1, true, 1000, "Л. Н. Толстой"),
+        Book("Капитанская дочка", 2, true, 250, "А. С. Пушкин"),
+        Book("Преступление и наказание", 3, false, 430, "Ф. М. Достоевский"),
+        Book("Мастер и Маргарита", 4, true, 480, "М. А. Булгаков"),
+        Book("Анна Каренина", 5, true, 864, "Л. Н. Толстой"),
+        Book("Евгений Онегин", 6, true, 320, "А. С. Пушкин")
     )
     val listOfNewspapers = listOf(
-        Newspaper(794, "Сельская жизнь", 17245, true),
-        Newspaper(501, "Комсомольская правда", 99, true),
-        Newspaper(302, "Московский комсомолец", 120, false),
-        Newspaper(456, "Аргументы и факты", 200, true),
-        Newspaper(789, "Известия", 150, true),
-        Newspaper(123, "Российская газета", 300, true)
+        Newspaper("Сельская жизнь", 794, true, 17245),
+        Newspaper("Комсомольская правда", 99, true, 501),
+        Newspaper("Московский комсомолец", 120, false, 302),
+        Newspaper("Аргументы и факты", 200, true, 456),
+        Newspaper("Известия", 150, true, 789),
+        Newspaper("Российская газета", 300, true, 123)
     )
 
     val listOfDisks = listOf(
-        Disk("DVD", "Дэдпул и Росомаха", 52, true),
-        Disk("DVD", "Терминатор", 348, false),
-        Disk("DVD", "Интерстеллар", 169, true),
-        Disk("CD", "Начало", 148, true),
-        Disk("DVD", "Матрица", 136, true),
-        Disk("CD", "Гладиатор", 155, true)
+        Disk("Дэдпул и Росомаха", 52, true, "DVD"),
+        Disk("Терминатор", 348, false, "DVD"),
+        Disk("Интерстеллар", 169, true, "DVD"),
+        Disk("Начало", 148, true, "CD"),
+        Disk("Матрица", 136, true, "DVD"),
+        Disk("Гладиатор", 155, true, "CD")
     )
 
     mainMenu(listOfBooks, listOfNewspapers, listOfDisks)
